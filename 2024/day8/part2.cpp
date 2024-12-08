8d6cf5b6418b3cb07811c50da291114ec518573a
gaspardd <gaspard.dannet@gmail.com>
2024-12-08 21:56:09 +0100

    day8, part2

--- 2024/day8/part2.cpp
+++ 2024/day8/part2.cpp
@@ -16,31 +16,32 @@ void print_vect_ant(vector<tuple<char,int,int>>& vect_ant) {
     }
 }
 
-bool list_contains(list<char> l, char c) {
-    bool b = (find(l.begin(), l.end(), c) != l.end());
-    return b;
+void print_antinode(set<pair<int,int>>& set_antinode) {
+    for (pair<int,int> p:set_antinode) {
+        cout << p.first<<", "<<p.second<<endl;
+    }
 }
 
-vector<tuple<char,int,int>> get_ant_char(vector<tuple<char,int,int>>& vect_ant, list<char>& ant_checked) {
+vector<tuple<char,int,int>> get_ant_char(vector<tuple<char,int,int>>& vect_ant, set<char>& ant_checked) {
     vector<tuple<char,int,int>> ant_not_checked;
 
     for (tuple<char,int,int> t: vect_ant) {
         char ant = get<0>(t);
-        if (!(list_contains(ant_checked, ant))) {
+        if (!ant_checked.count(ant)) {
             for (tuple<char,int,int> t:vect_ant) {
                 char c = get<0>(t);
                 if (c==ant) {
                     ant_not_checked.push_back(t);
                 }
             }
-            ant_checked.push_back(ant);
+            ant_checked.insert(ant);
             break;
         }
     }
     return ant_not_checked;
 }
 
-set<pair<int,int>> place_antinode(vector<tuple<char,int,int>> ant_not_checked, int& n, int& m, set<pair<int,int>>& list_antinode) {
+set<pair<int,int>> place_antinode(vector<tuple<char,int,int>> ant_not_checked, int& n, int& m, set<pair<int,int>>& set_antinode) {
     int i1,j1,i2,j2,pos1i,pos1j,pos2i,pos2j,idiff,jdiff;
     for (long unsigned int i=0; i<ant_not_checked.size()-1;i++) {
         for (long unsigned int j=i+1; j<ant_not_checked.size(); j++) {
@@ -48,19 +49,24 @@ set<pair<int,int>> place_antinode(vector<tuple<char,int,int>> ant_not_checked, i
             tuple<char,int,int> ant2 = ant_not_checked[j];
             i1 = get<1>(ant1); j1 = get<2>(ant1);
             i2 = get<1>(ant2); j2 = get<2>(ant2);
+            set_antinode.insert(make_pair(i1,j1));
+            set_antinode.insert(make_pair(i2,j2));
 
             idiff = i1-i2; jdiff = j1-j2;
             pos1i = i1+idiff; pos1j = j1+jdiff;
-            pos2i = i1-2*idiff; pos2j = j1-2*jdiff;
-            if (0<=pos1i && pos1i<n && 0<=pos1j && pos1j <m) {
-                list_antinode.insert(make_pair(pos1i,pos1j));
+            while (0<=pos1i && pos1i<n && 0<=pos1j && pos1j <m) {
+                set_antinode.insert(make_pair(pos1i,pos1j));
+                pos1i += idiff; pos1j += jdiff;
+
             }
-            if (0<=pos2i && pos2i<n && 0<=pos2j && pos2j <m) {
-                list_antinode.insert(make_pair(pos2i,pos2j));
+            pos2i = i1-2*idiff; pos2j = j1-2*jdiff;
+            while (0<=pos2i && pos2i<n && 0<=pos2j && pos2j <m) {
+                set_antinode.insert(make_pair(pos2i,pos2j));
+                pos2i -= idiff; pos2j -= jdiff;
             }
         }
     }
-    return list_antinode;
+    return set_antinode;
 }
 
 int main() { 
@@ -82,7 +88,6 @@ int main() {
             c = line[k];
             mat_ln.push_back(c);
             if (c != '.') {
-                // cout <<"c: " <<c<<", i:"<< i<<", j:"<<j<<endl;
                 vec_ant.push_back(make_tuple(c,i,j));
             }
             j++;
@@ -92,7 +97,7 @@ int main() {
         i++;
     }
 
-    list<char> ant_checked;
+    set<char> ant_checked;
     vector<tuple<char,int,int>> ant_not_checked;
 
     int n = mat.size();
@@ -102,11 +107,12 @@ int main() {
     ant_not_checked = get_ant_char(vec_ant, ant_checked);
     while (!(ant_not_checked.empty())) {
         place_antinode(ant_not_checked,n,m,set_antinode);
-        // print_vect_ant(ant_not_checked);
         ant_not_checked = get_ant_char(vec_ant, ant_checked);
     }
 
     int nb_anti_node = set_antinode.size();
+    // cout<<"antinodes: "<<endl;
+    // print_antinode(set_antinode); 
     cout << "result: "<<nb_anti_node << endl;
 
     return 0;
